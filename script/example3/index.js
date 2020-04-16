979e21ad6951988fb53101c3fc33329803a3a09a
ozen-lin <ozen.lin@haydn-city.com>
2020-04-16 12:06:04 +0800

    完成腳本

--- script/example3/index.js
+++ script/example3/index.js
@@ -1,4 +1,3 @@
-
 new KonvaMG.Stage({ container: 'container', width: 800, height: 600 }, ThisStage => {
 
     const vw = ThisStage.Stage.getWidth();
@@ -26,12 +25,12 @@ new KonvaMG.Stage({ container: 'container', width: 800, height: 600 }, ThisStage
         })
 
     ThisStage
-        .at({ time: "00:1.6", add: "Rect", inLayer: 1 }, {
+        .at({ time: "00:01.6", add: "Rect", inLayer: 1 }, {
             x: vw,
             y: vh / 2 - 45 - 10,
             width: 0,
             height: 90,
-            fill: '#FCF9AA',
+            fill: '#FCF9AA'
         })
         .andTween({ after: "00:00" }, {
             duration: 0.6,
@@ -40,29 +39,81 @@ new KonvaMG.Stage({ container: 'container', width: 800, height: 600 }, ThisStage
         })
 
     ThisStage
-        .at({ time: "00:2", add: "Ring", inLayer: 0 }, {
+        .at({ time: "00:02", add: "Ring", inLayer: 0 }, {
             x: vw / 2,
             y: vh / 2,
             innerRadius: 0,
             outerRadius: 0,
-            fill: '#8BCD96',
+            fill: '#8BCD96'
         })
         .andTween({ after: "00:00" }, {
             duration: 0.2,
-            outerRadius: 180,
+            outerRadius: 180
         })
-        .andTween({ after: "00:0.2" }, {
+        .andTween({ after: "00:00.2" }, {
             duration: 0.2,
-            innerRadius: 180,
+            innerRadius: 180
         })
-        .andTween({ after: "00:0.4" }, {
-            fill: '#AEFFBC',
+        .andTween({ after: "00:00.4" }, {
             duration: 5.6,
-            easing: Konva.Easings['Linear'],
+            fill: '#AEFFBC',
+            easing: Konva.Easings.Linear,
             innerRadius: 219,
-            outerRadius: 220,
+            outerRadius: 220
         })
 
+    const pos = 300;
+    const pos2 = -400;
+    const size = 1000;
 
+    ThisStage
+        .at({ time: "00:03.6", add: "Rect", inLayer: 3 }, {
+            fill: '#fff',
+            x: -pos,
+            y: pos,
+            width: 0,
+            height: size,
+            rotation: -45
+        })
+        .andTween({ after: "00:00" }, {
+            duration: 0.1,
+            width: size
+        })
+        .andTween({ after: "00:00.13" }, {
+            duration: 0.1,
+            width: 0,
+            x: -pos2,
+            y: pos2
+        })
+
+    ThisStage
+        .at({ time: "00:03.7", add: "Text", inLayer: 2 }, {
+            X: vw / 2 - 128,
+            y: vh / 2 - 92,
+            text: 'OZEN',
+            fontSize: 160,
+            fontStyle: 'bold',
+            fontFamily: 'Martin Amitrano',
+            fill: '#414141'
+        })
+
+    ThisStage
+        .at({ time: "00:04.5", add: "Wedge", inLayer: 0 }, {
+            x: vw / 2,
+            y: vh / 2,
+            radius: 200,
+            angle: 0,
+            fill: '#AEFFBC',
+            rotation: -45
+        })
+        .andTween({ after: "00:00" }, {
+            duration: 0.2,
+            angle: 360
+        })
+        .andTween({ after: "00:00.3" }, {
+            duration: 0.5,
+            angle: 0,
+            rotation: -45 + 360
+        })
 
 })
